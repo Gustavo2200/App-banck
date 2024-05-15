b86ca4f526a2dc4ec82f5ab8c3a8cb101bc37a84
EduardoBabetto <edubabetto@gmail.com>
2024-05-15 17:58:13 -0300

    Criado os arquivos da tela-principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TelaPrincipalComponent } from './components/tela-principal/tela-principal.component';
+import { CabecalhoComponent } from './components/tela-principal/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/tela-principal/rodape/rodape.component';
+import { ExtratoComponent } from './components/tela-principal/extrato/extrato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaPrincipalComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    ExtratoComponent
   ],
   imports: [
     BrowserModule,
