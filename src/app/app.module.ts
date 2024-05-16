ebfb0917abad76eca6ac26bed846f1a1f72f2ac8
davisoles <drbp.1306@gmail.com>
2024-05-16 17:00:08 -0300

    pagina login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CabecalhoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
