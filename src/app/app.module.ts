470394ccbc78eba5c7c58b919011e0d27c7ddb9b
Thiago <thiago_dev@companye.academy>
2024-05-16 16:12:53 -0400

    ajuste de config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,7 @@ import { ModalDestinatarioPixComponent } from './components/modal-destinatario-p
 import { LoginComponent } from './components/login/login.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
-
 import { TelaPrincipalComponent } from './components/tela-principal/tela-principal.component';
-import { CabecalhoComponent } from './components/tela-principal/cabecalho/cabecalho.component';
-import { RodapeComponent } from './components/tela-principal/rodape/rodape.component';
 import { ExtratoComponent } from './components/tela-principal/extrato/extrato.component';
 import { CadastroClienteComponent } from './components/cadastro-cliente/cadastro-cliente.component';
 import { FormsModule } from '@angular/forms';
