5c0dd7a8f92fd18e1469ca315389d3b87e0cd965
Thiago <thiago_dev@companye.academy>
2024-05-17 15:12:38 -0400

    add service para cadastro de cliente

--- src/app/components/cadastro-cliente/cadastro-cliente.component.ts
+++ src/app/components/cadastro-cliente/cadastro-cliente.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NovoCliente } from '../../interfaces/request/NovoCliente';
+import { ClienteService } from '../../service/cliente.service';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-cadastro-cliente',
@@ -8,6 +10,8 @@ import { NovoCliente } from '../../interfaces/request/NovoCliente';
 })
 export class CadastroClienteComponent {
   
+  constructor(private clienteService: ClienteService) { }
+
   novoCliente: NovoCliente = {
     name: '',
     email: '',
@@ -18,6 +22,23 @@ export class CadastroClienteComponent {
   }
 
   cadastrar() {
-    alert(this.novoCliente.name + '\nCadastrado com sucesso!');
-  }
+    this.clienteService.cadastrarCliente(this.novoCliente)
+      .pipe(
+        catchError(error => {
+          if(error.status != 201) {
+            alert(error.message);
+          }
+          return error;
+        })   
+      );
+      alert("Cadastrado com sucesso!");
+      this.novoCliente = {
+        name: '',
+        email: '',
+        phone: '',
+        password: '',
+        cpf: '',
+        dateBirth: new Date()
+      }
+    }
 }
