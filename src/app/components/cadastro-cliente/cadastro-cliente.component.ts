1f4289fd563ea37661a64a547f2f7208c2eafbdd
Thiago <thiago_dev@companye.academy>
2024-05-20 15:04:46 -0400

    ajustes cadastro de cliente

--- src/app/components/cadastro-cliente/cadastro-cliente.component.ts
+++ src/app/components/cadastro-cliente/cadastro-cliente.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NovoCliente } from '../../interfaces/request/NovoCliente';
 import { ClienteService } from '../../service/cliente.service';
 import { catchError } from 'rxjs';
+import { ErroResponse } from '../../interfaces/response/ErroResponse';
 
 @Component({
   selector: 'app-cadastro-cliente',
@@ -9,7 +10,7 @@ import { catchError } from 'rxjs';
   styleUrl: './cadastro-cliente.component.css'
 })
 export class CadastroClienteComponent {
-  
+
   constructor(private clienteService: ClienteService) { }
 
   novoCliente: NovoCliente = {
@@ -21,24 +22,50 @@ export class CadastroClienteComponent {
     dateBirth: new Date()
   }
 
+  erros: ErroResponse[] = [];
+
+  camposNulos: String[] = [];
+
   cadastrar() {
-    this.clienteService.cadastrarCliente(this.novoCliente)
-      .pipe(
-        catchError(error => {
-          if(error.status != 201) {
-            alert(error.message);
+    this.validarCampos();
+    if (this.camposNulos.length == 0) {
+      this.clienteService.cadastrarCliente(this.novoCliente)
+        .subscribe(
+          () => {
+            alert('Cadastrado com sucesso!');
+          },
+          (error: ErroResponse[]) => {
+            alert(error.map(error => error.message).join('\n'));
+            this.erros = error
           }
-          return error;
-        })   
-      );
-      alert("Cadastrado com sucesso!");
-      this.novoCliente = {
-        name: '',
-        email: '',
-        phone: '',
-        password: '',
-        cpf: '',
-        dateBirth: new Date()
-      }
+        )
+    }else{
+      alert(this.camposNulos.join('\n'));
     }
+  }
+
+  validarCampos() {
+    this.camposNulos = [];
+    if (this.novoCliente.name == '') {
+      this.camposNulos.push('Nome obrigatório');
+    }
+
+    if (this.novoCliente.email == '') {
+      this.camposNulos.push('Email obrigatório');
+    }
+
+    if (this.novoCliente.phone == '') {
+      this.camposNulos.push('Telefone obrigatório');
+    }
+
+    if (this.novoCliente.password == '') {
+      this.camposNulos.push('Senha obrigatório');
+    }
+
+    if (this.novoCliente.cpf == '') {
+      this.camposNulos.push('CPF obrigatório');
+    }
+
+  }
+
 }
--- src/app/service/cliente.service.ts
+++ src/app/service/cliente.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { NovoCliente } from '../interfaces/request/NovoCliente';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { ErroResponse } from '../interfaces/response/ErroResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,25 @@ export class ClienteService {
 
   cadastrarCliente(cliente: NovoCliente): Observable<any> {
     return this.http.post(this.api+"/save-customer", cliente)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
+  private handleError(error: HttpErrorResponse): Observable<ErroResponse[]> {
+    let errorMessage: ErroResponse[] = [];
 
+    if (error.error instanceof Array) {
+      errorMessage = error.error;
+    } else {
+      errorMessage = [{
+        message: error.message,
+        status: error.status,
+        path: error.url || '',
+        timestamp: new Date().toISOString()
+      }];
+    }
+
+    return throwError(errorMessage);
+  }
 }
