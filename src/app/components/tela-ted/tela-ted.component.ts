d5fc01a4f800728becfff166f1149a03a5650216
joao-vitor00 <joaovitorscarvalho2007@gmail.com>
2024-05-21 16:55:11 -0300

    atualizando resposta

--- src/app/components/tela-ted/tela-ted.component.ts
+++ src/app/components/tela-ted/tela-ted.component.ts
@@ -13,7 +13,7 @@ export class TelaTedComponent {
 
   constructor(private infoTedService: InfoTedService) { }
 
-  token: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmb3VyYmFuayIsInN1YiI6IjgwMTAzOTE5MDU4IiwiY3VzdG9tZXJfaWQiOiIxNyIsImNwZiI6IjgwMTAzOTE5MDU4IiwicGFzc3dvcmQiOiIkMmEkMTAkS3pCclBoODltWGl3VGJCc3F5LzlZT0pXY1pZLlFxOGdvT0ZrWmZNZHVlTEYwR2JPU3FDR3kiLCJleHAiOjE3MTYyMzgwMzN9.SId03etr9_R2zNpbZ9Uju3MSLeURFPtRI21GVMGWnHg";
+  token: string = "";
   
   
   infoTed: InfoTed = {
@@ -25,7 +25,11 @@ export class TelaTedComponent {
   transferir() {
     if(this.infoTed.agency.length == 4 && this.infoTed.accountNumber.length == 8 && this.infoTed.value.valueOf() > 0) {
       
-    if(this.infoTedService.transferir(this.infoTed, this.token).subscribe()) {
+    if(this.infoTedService.transferir(this.infoTed, this.token).subscribe(
+      (response ) => {
+        alert(response)
+      }, (error) => alert(error)
+    )) {
         alert("Transferência efetuada com sucesso!")
       }else{
         alert("Erro ao efetuar transferência!")
