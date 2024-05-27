ed02387828eee24142cea3b8b38f4e49321af011
Gustavo2200 <gustavoparreiras123pie@gmail.com>
2024-05-27 14:32:32 -0300

    Ajuste rodape/cabecalho

--- src/app/components/tela-principal/tela-principal.component.ts
+++ src/app/components/tela-principal/tela-principal.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Conta } from '../../interfaces/request/Conta';
 import { ContaService } from '../../service/conta.service';
+import { ComponenteSelecionadoService } from '../../service/componente-selecionado.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tela-principal',
@@ -18,8 +20,16 @@ export class TelaPrincipalComponent {
   mostrarSaldo: boolean = true;
   componenteAtual: string = 'extrato'
 
-  constructor(private contaService: ContaService){}
+  constructor(
+    private contaService: ContaService, 
+    private componenteService: ComponenteSelecionadoService,
+    private route: ActivatedRoute
+
+  ){}
 ngOnInit(): void {
+  this.componenteService.componenteAtual$.subscribe(componente => {
+    this.componenteAtual = componente; 
+  });
   this.contaService.accountInfo().subscribe((getConta)=>{
     this.conta = getConta;
   })
